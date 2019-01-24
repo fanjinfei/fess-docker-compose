994952b88d985da400ecf1334b2b0a8fba361011
Jinfei Fan <fanjinfei@hotmail.com>
2019-01-24 13:24:39 -0500

    restore config on boot

--- data/bin/restore_config.sh
+++ data/bin/restore_config.sh
@@ -1,4 +1,15 @@
 #!bin/sh
 
-elasticdump --input=http://localhost:9200/.fess_config.label_type --output=/tmp/label.json
+rm -rf /tmp/config
+mkdir -p /tmp/config
+cp -f /var/lib/elasticsearch/backup/* /tmp/config/
+
+elasticdump --output=http://localhost:9200/.fess_config.data_config --input=/tmp/config/datastore.json
+elasticdump --output=http://localhost:9200/.fess_config.label_type --input=/tmp/config/label.json
+elasticdump --output=http://localhost:9200/.fess_config.data_config_to_label --input=/tmp/config/data_config_to_label.json
+elasticdump --output=http://localhost:9200/.fess_config.scheduled_job --input=/tmp/config/scheduled_job.json
+elasticdump --output=http://localhost:9200/.fess_config.crawling_info --input=/tmp/config/crawl.json
+elasticdump --output=http://localhost:9200/.fess_config.key_match --input=/tmp/config/key_match.json
+
+rm -rf /tmp/config
 
